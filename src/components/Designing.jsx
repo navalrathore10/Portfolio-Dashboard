7a0b82301f2947349f097a3d2c4084e27be2bcef
navalrathore10 <rathorenaval1998@gmail.com>
2025-03-16 23:05:53 +0530

    Brezza Color Change Module Added,

--- src/components/Designing.jsx
+++ src/components/Designing.jsx
@@ -6,13 +6,14 @@ import { motion } from 'framer-motion';
 export default function Designing() {
     const { toggled } = useToggle();
     const [carColor, setCarColor] = useState('');
+    const [brezzaColor, setBrezzaColor] = useState('');
 
     return (<>
         <motion.div initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 1.3 }}>
-            <div className={`pb-5 px-5 md:pb-11 mt-5 rounded-3xl w-full ${toggled ? 'bg-dark24' :
+            <div className={`pb-5 px-5 md:pb-11 mt-5 grid gap-5 rounded-3xl w-full ${toggled ? 'bg-dark24' :
                 carColor == 'blue' ? 'bg-blue-200' :
                     carColor == 'green' ? 'bg-green-200' :
                         carColor == 'pink' ? 'bg-red-200' :
@@ -24,7 +25,7 @@ export default function Designing() {
                     <img src="https://github.com/navalrathore10/Picload-Naval-Rathore/blob/master/src/assets/Untitled-11.png?raw=true"
                         alt="" width={'100%'} className={`${toggled ? 'invert' : ''} `} />
                 </div>
-                <div className="sports">
+                <div className="sports mb-12">
                     <div className="car-image px-11 w-full h-min">
                         <img src="https://github.com/navalrathore10/Picload-Naval-Rathore/blob/master/src/assets/NRZ-2000.png?raw=true"
                             alt="" width={'100%'}
@@ -57,26 +58,26 @@ export default function Designing() {
                         <img src="https://github.com/navalrathore10/Portfolio-Dashboard/blob/main/public/img/brezza.png?raw=true"
                             alt="" width={'100%'}
                             className={`
-                            ${carColor == 'blue' ? 'hue-rotate-[155deg]' :
-                                    carColor == 'green' ? 'hue-rotate-[105deg]' :
-                                        carColor == 'pink' ? 'hue-rotate-[295deg]' :
+                            ${brezzaColor == 'blue' ? 'hue-rotate-[575deg]' :
+                                    brezzaColor == 'green' ? 'hue-rotate-[85deg]' :
+                                        brezzaColor == 'pink' ? 'hue-rotate-[295deg]' :
                                             'hue-rotate-[0deg]'
                                 }
                         
                         ${toggled ? '' : ''} `} />
                     </div>
                     <div className="bnts flex gap-2 justify-center mt-5">
-                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#13c6ff] 155 hover:w-14  duration-300 transition-ease-in'
-                            onClick={() => { setCarColor('blue') }}
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[rgb(0_62_169)] 155 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setBrezzaColor('blue') }}
                         ></button>
-                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#00dc75] 105 hover:w-14  duration-300 transition-ease-in'
-                            onClick={() => { setCarColor('green') }}
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#337100] 105 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setBrezzaColor('green') }}
                         ></button>
-                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff6fb6] 295 hover:w-14  duration-300 transition-ease-in'
-                            onClick={() => { setCarColor('pink') }}
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#a019b1] 295 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setBrezzaColor('pink') }}
                         ></button>
-                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff990d] 0 hover:w-14  duration-300 transition-ease-in'
-                            onClick={() => { setCarColor('orange') }}
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ca0000] 0 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setBrezzaColor('orange') }}
                         ></button>
                     </div>
                 </div>
