0a80bed37000d8091046811917b310c1dfd7a411
navalrathore10 <rathorenaval1998@gmail.com>
2025-03-16 22:38:19 +0530

    New Car Added (Brezza)

--- src/components/Designing.jsx
+++ src/components/Designing.jsx
@@ -24,31 +24,61 @@ export default function Designing() {
                     <img src="https://github.com/navalrathore10/Picload-Naval-Rathore/blob/master/src/assets/Untitled-11.png?raw=true"
                         alt="" width={'100%'} className={`${toggled ? 'invert' : ''} `} />
                 </div>
-                <div className="car-image px-11 w-full h-min">
-                    <img src="https://github.com/navalrathore10/Picload-Naval-Rathore/blob/master/src/assets/NRZ-2000.png?raw=true"
-                        alt="" width={'100%'}
-                        className={`
+                <div className="sports">
+                    <div className="car-image px-11 w-full h-min">
+                        <img src="https://github.com/navalrathore10/Picload-Naval-Rathore/blob/master/src/assets/NRZ-2000.png?raw=true"
+                            alt="" width={'100%'}
+                            className={`
                             ${carColor == 'blue' ? 'hue-rotate-[155deg]' :
-                                carColor == 'green' ? 'hue-rotate-[105deg]' :
-                                    carColor == 'pink' ? 'hue-rotate-[295deg]' :
-                                        'hue-rotate-[0deg]'
-                            }
+                                    carColor == 'green' ? 'hue-rotate-[105deg]' :
+                                        carColor == 'pink' ? 'hue-rotate-[295deg]' :
+                                            'hue-rotate-[0deg]'
+                                }
                         
                         ${toggled ? '' : ''} `} />
+                    </div>
+                    <div className="bnts flex gap-2 justify-center mt-5">
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#13c6ff] 155 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('blue') }}
+                        ></button>
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#00dc75] 105 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('green') }}
+                        ></button>
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff6fb6] 295 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('pink') }}
+                        ></button>
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff990d] 0 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('orange') }}
+                        ></button>
+                    </div>
                 </div>
-                <div className="bnts flex gap-2 justify-center mt-5">
-                    <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#13c6ff] 155 hover:w-14  duration-300 transition-ease-in'
-                        onClick={() => { setCarColor('blue') }}
-                    ></button>
-                    <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#00dc75] 105 hover:w-14  duration-300 transition-ease-in'
-                        onClick={() => { setCarColor('green') }}
-                    ></button>
-                    <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff6fb6] 295 hover:w-14  duration-300 transition-ease-in'
-                        onClick={() => { setCarColor('pink') }}
-                    ></button>
-                    <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff990d] 0 hover:w-14  duration-300 transition-ease-in'
-                        onClick={() => { setCarColor('orange') }}
-                    ></button>
+                <div className="brezza">
+                    <div className="car-image px-11 w-full h-min">
+                        <img src="https://github.com/navalrathore10/Picload-Naval-Rathore/blob/master/src/assets/NRZ-2000.png?raw=true"
+                            alt="" width={'100%'}
+                            className={`
+                            ${carColor == 'blue' ? 'hue-rotate-[155deg]' :
+                                    carColor == 'green' ? 'hue-rotate-[105deg]' :
+                                        carColor == 'pink' ? 'hue-rotate-[295deg]' :
+                                            'hue-rotate-[0deg]'
+                                }
+                        
+                        ${toggled ? '' : ''} `} />
+                    </div>
+                    <div className="bnts flex gap-2 justify-center mt-5">
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#13c6ff] 155 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('blue') }}
+                        ></button>
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#00dc75] 105 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('green') }}
+                        ></button>
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff6fb6] 295 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('pink') }}
+                        ></button>
+                        <button className=' w-5 h-5 md:w-10 md:h-10 border-2 border-white rounded-full bg-[#ff990d] 0 hover:w-14  duration-300 transition-ease-in'
+                            onClick={() => { setCarColor('orange') }}
+                        ></button>
+                    </div>
                 </div>
             </div>
             <div className={`text-center w-full mt-5 text-syne ${toggled ? 'text-dark24' : 'text-lighte3'}`}> More Coming Soon</div>
